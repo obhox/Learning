5e414fe8154186e54b9b3d33fb1c25e14da575e6
Obhox <oguntonajoy@gmail.com>
2024-08-14 23:04:56 +0100

    Last learning 11:04pm

--- guessing.py
+++ guessing.py
@@ -1,2 +1,29 @@
+import random
+import sys
 
-        
+def guessNumber():
+    print("I am thinking of a number between 1 and 20")
+    
+    player = int(input("Take a guess: "))
+    computer = random.randint(1, 20)
+    
+    if player == computer:
+        print("Good job")
+    elif player < computer:
+        print(f"Your guess is too low, computer guess is {computer}")
+    else: 
+        print(f"Your guess is too high, computer guess is {computer}")
+    
+    return input("\nWill you be interested in playing again? (y/n)\n").lower()
+
+count = 0
+while True:
+    count += 1
+    play_again = guessNumber()
+    
+    if play_again != 'y':
+        print(f"\nYou have played {count} time(s)")
+        sys.exit()
+
+
+guessNumber()
---
+++ looptest.py
@@ -0,0 +1,9 @@
+i = 0
+
+while i < 10:
+    i = i + 1
+    print(i)
+
+for i in range(0,10):
+    i = i+1
+    print (i)
